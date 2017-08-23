e27b5b2359d11c469575c44e9562442b7b6f5c11
nibons <imac5640@gmail.com>
2017-08-23 10:48:18 -0400

    added initial profile files

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,45 @@
+Write-Information "Starting PowerShell Profile"
+$env:docker_host = 'tcp://nanocontainers:2375'
+if ($pwd.path -eq "$($env:SystemRoot)\system32") {Set-Location $HOME}
+#$HBEcredential = get-credential -Message "chfs\clay.haertzen Credentials"
+function prompt {"[$(get-date -format 'HH:mm:ss')] [{0}]>" -f $PWD.Path}
+$servername_ProdDeploy = "HFSNE121-037436.chfs.ds.ky.gov"
+$hbeCredential = Import-Clixml -Path "$home\HBECredential.xml"
+$dockerCredential = import-clixml -path "$home\dockerCredential.xml"
+
+if (Test-Connection -ComputerName $servername_ProdDeploy -Count 1 -Quiet) {
+    if (!(test-path e:\) -and !(get-psdrive | Where-Object name -eq 'E')) {New-PSDrive -Name E -PSProvider FileSystem -Root "\\$servername_ProdDeploy\e$" -Persist -Credential $hbeCredential} 
+    if (!(get-psdrive | Where-Object name -eq 'scripts')) {new-psdrive -Name Scripts -PSProvider FileSystem -Root "\\$servername_ProdDeploy\scripts" -Credential $hbeCredential| Out-Null}
+    Import-Module Scripts:\Get-Servers.psm1
+}
+#set CredSSP to enabled
+
+Function switch-ContainerOSArch {
+    & 'C:\Program Files\Docker\Docker\DockerCli.exe' -SwitchDaemon
+}
+
+#region GIT
+if (Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
+# Load posh-git example profile
+. 'C:\tools\poshgit\dahlbyk-posh-git-fba883f\profile.example.ps1'
+
+
+
+
+Rename-Item Function:\Prompt PoshGitPrompt -Force
+function Prompt() {
+    if (Test-Path Function:\PrePoshGitPrompt) {
+        ++$global:poshScope
+        New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline)" -Force | Out-Null
+        $private:p = PrePoshGitPrompt
+        if (--$global:poshScope -eq 0) {
+            Remove-Item function:\Write-Host -Force
+        }
+    }
+    PoshGitPrompt
+}
+
+. "$psscriptRoot\setup_GIT.ps1"
+#endregion
+
+#clear-host
---
+++ setup_GIT.ps1
@@ -0,0 +1,30 @@
+﻿# Update path for SSH (Loaded in PowerShell Profile)
+if($env:path -notmatch "Git\\bin"){ $env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\bin"}
+if($env:path -notmatch "Git\\usr\\bin"){$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\usr\bin"}
+
+# Load SSH agent utils
+#. (Resolve-Path ~/Documents/WindowsPowershell/ssh-agent-utils.ps1)
+
+# Spoof terminal environment for git color.
+$env:TERM = 'cygwin'
+
+# Load posh-git example profile, which will setup a prompt
+#. 'C:\tools\poshgit\*-posh-git-*\profile.example.ps1'
+Import-Module posh-git
+$env:ShowGit = $true
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    Write-Host($pwd.ProviderPath) -nonewline
+    if($env:ShowGit){
+        Write-VcsStatus
+    }
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "> "
+}
+Start-SshAgent -Quiet
+
+
+Pop-Location
+
+Add-SshKey
